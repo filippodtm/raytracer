ef03b39c2c2da32e4eb1991d1a00b681c2f0acc5
filippo <filippo.ditommaso98@gmail.com>
2022-12-01 12:55:09 +0100

    fixed scene1.py "transformation"

--- scene1.py
+++ scene1.py
@@ -11,13 +11,13 @@ from myworld import *
 w = World()
 
 floor = sphere()
-floor.transform = Matrix.scaling(10, .01, 10)
+floor.transformation = Matrix.scaling(10, .01, 10)
 floor.material = Material()
 floor.material.color = Color(1, .9, .9)
 floor.material.specular = 0
 
 leftwall = sphere()
-leftwall.transform = Matrix.translation(0,0,5) * Matrix.yrotation(-pi
+leftwall.transformation = Matrix.translation(0,0,5) * Matrix.yrotation(-pi
                     /4) *Matrix.xrotation(pi/2) * Matrix.scaling(10, .01, 10)
 #leftwall.material = floor.material
 leftwall.material.color = Color(1, .9, .9)
@@ -25,25 +25,25 @@ leftwall.material.specular = 0
 
 
 rightwall = sphere()
-rightwall.transform = Matrix.translation(0,0,5) * Matrix.yrotation(pi
+rightwall.transformation = Matrix.translation(0,0,5) * Matrix.yrotation(pi
                                         /4) *Matrix.xrotation(pi/2) * Matrix.scaling(10, .01, 10)
 rightwall.material = floor.material
 
 
 middle = sphere()
-middle.transform = Matrix.translation(-.5, 1, .5) #* Matrix.shear(1,1, 0,1, 0,0)
+middle.transformation = Matrix.translation(-.5, 1, .5) #* Matrix.shear(1,1, 0,1, 0,0)
 middle.material.color = Color(.1, 1, .5)
 middle.material.diffuse = 0.7
 middle.material.specular = 1 #
 
 right = sphere()
-right.transform = Matrix.translation(1.5, 0.5, -0.5) * Matrix.scaling(.5, .5, .5)
+right.transformation = Matrix.translation(1.5, 0.5, -0.5) * Matrix.scaling(.5, .5, .5)
 right.material.color = Color(.5, 1, .1)
 right.material.diffuse = 0.7
 right.material.specular = 0.3
 
 left = sphere()
-left.transform = Matrix.translation(-1.5, 0.33, -0.75) * Matrix.scaling(.33, .33, .33)
+left.transformation = Matrix.translation(-1.5, 0.33, -0.75) * Matrix.scaling(.33, .33, .33)
 left.material.color = Color(1, .4, .1) #
 left.material.diffuse = 0.7
 left.material.specular = 0.5 #
@@ -58,7 +58,7 @@ def scene1(n):
     start = datetime.now()
     
     camera = Camera(n, round(3/4*n), pi/3)
-    camera.transform = Matrix.viewtransform(Point(0, 1.5, -5), Point(0,1,0), Vector(0,1,0))
+    camera.transformation = Matrix.viewtransform(Point(0, 1.5, -5), Point(0,1,0), Vector(0,1,0))
     canvas = camera.render(w)
     canvastoppm(canvas, 'scene_1.ppm')
 
